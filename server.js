c69c51c6745e987907f3e81660be88f8deeda80a
Chris Tsongas <chris.tsongas@gmail.com>
2018-06-23 15:55:10 +0000

    Prettier

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 if (require.main === module) {
-  app.listen(process.env.PORT || 8080, function () {
+  app.listen(process.env.PORT || 8080, function() {
     console.info(`App listening on ${this.address().port}`);
   });
 }
--- test/test-server.js
+++ test/test-server.js
@@ -1,18 +1,19 @@
-'use strict';
+"use strict";
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../server.js');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../server.js");
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('index page', function () {
-  it('should exist', function () {
-    return chai.request(app)
-      .get('/')
-      .then(function (res) {
+describe("index page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get("/")
+      .then(function(res) {
         expect(res).to.have.status(200);
       });
   });
